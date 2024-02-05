b2a3b21af1a64c4535e7f4f9e7e4ce03dbb6c03f
ponomoly <ponomoly.dev@gmail.com>
2024-02-05 17:08:54 +0900

    feat: 색상 관련 constants 작성

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+import { DEFAULT_COLORS } from "./theme/colors";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      ...DEFAULT_COLORS,
+    },
     extend: {},
   },
   plugins: [],
